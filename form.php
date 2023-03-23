463571aedae700906489fbb0d6d97a91765c9157
colineguerin <guerin.coline@gmail.com>
2023-03-23 12:15:28 +0100

    quête form PHP n°2 avec sécurisation front et back

--- form.php
+++ form.php
@@ -11,23 +11,20 @@
 <form action="thanks.php" method="post">
     <div>
         <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="last_name">
+        <input type="text" required id="nom" name="last_name">
     </div>
     <div>
         <label for="prénom">Prénom :</label>
-        <input type="text" id="prénom" name="first_name">
+        <input type="text" required id="prénom" name="first_name">
     </div>
-
     <div>
         <label for="courriel">Courriel :</label>
-        <input type="email" id="courriel" name="user_email">
+        <input type="email" required id="courriel" name="user_email">
     </div>
-
     <div>
         <label for="telephone">Téléphone :</label>
-        <input type="tel" id="telephone" name="user_phone">
+        <input type="tel" required id="telephone" name="user_phone">
     </div>
-
     <div>
         <label for="sujet">Sujet :</label>
         <select id="sujet" name="user_topic">
@@ -36,16 +33,17 @@
             <option value="remboursement">Je veux me faire rembourser</option>
         </select>
     </div>
-
     <div>
         <label for="message">Message :</label>
-        <textarea id="message" name="user_message" rows="5" cols="33"></textarea>
+        <textarea id="message" required name="user_message" rows="5" cols="33"></textarea>
     </div>
-
     <div class="button">
         <button type="submit">Envoyer votre message</button>
     </div>
-
 </form>
+
 </body>
 </html>
+
+
+
--- thanks.php
+++ thanks.php
@@ -1,7 +1,39 @@
+<?php
 
+$errors = [];
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(!isset($_POST['last_name']) || trim($_POST['last_name']) == ''){
+        $errors[] = 'Veuillez entrer votre nom';
+    }
+    if(!isset($_POST['first_name']) || trim($_POST['first_name']) == ''){
+        $errors[] = 'Veuillez entrer votre prénom';
+    }
+    if(!isset($_POST['user_email']) || trim($_POST['user_email']) == '' || filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)){
+        $errors[] = 'Veuillez entrer votre adresse mail';
+    }
+    if(!isset($_POST['user_phone']) || trim($_POST['user_phone']) == ''){
+        $errors[] = 'Veuillez entrer votre numéro de téléphone';
+    }
+    if(!isset($_POST['user_message']) || trim($_POST['user_message']) == ''){
+        $errors[] = 'Veuillez préciser votre demande';
+    }
+    if(count($errors)>0) { ?>
+        <div>
+            <?php foreach($errors as $error): ?>
+                <p><?= $error . '<br>' ?></p>
+            <?php endforeach; ?>
+        </div> <?php
+    }
+    if(empty($errors)) { ?>
 <div>
     <p>Merci <?= $_POST['first_name'] . ' ' . $_POST['last_name'] ?> de nous avoir contacté à propos de <?= $_POST['user_topic']?>.</p>
 
     <p>Un de nos conseillers vous contactera soit à l’adresse <?= $_POST['user_email']?> ou par téléphone au <?= $_POST['user_phone']?> dans les plus brefs délais pour traiter votre demande : <?= '<br><br>' . $_POST['user_message']?>
+</div> <?php
+    }
+}
+?>
 
-</div>
+
+
